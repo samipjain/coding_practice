b4a16cf72ea4092fe59c2b12db22913a6ecdaf2d
Samip Jain <samipjain1216@gmail.com>
2020-05-02 18:08:02 -0400

    Same with O(nlogn)

--- day2/checkifstringhasuniquecharacters.py
+++ day2/checkifstringhasuniquecharacters.py
@@ -1,3 +1,4 @@
+# Method 1 O(n2)
 def uniqueCharacters(s):
     for i in range(len(s)):
         for j in range(i+1, len(s)):
@@ -5,9 +6,22 @@ def uniqueCharacters(s):
                 return False
     return True
 
+# Method 2 O(nlogn)
+def uniqueCharacters2(s):
+    s1 = sorted(s)
+    for i in range(len(s1)-1):
+        if(s1[i] == s1[i+1]):
+            return False
+    return True
+
 s = "absc10gd"
 
 if uniqueCharacters(s):
+    print("Unique")
+else:
+    print("Not unique")
+
+if uniqueCharacters2(s):
     print("Unique")
 else:
     print("Not unique")
