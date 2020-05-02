f72b0fdab8dc716d0a87b0aba4762675c118d535
Samip Jain <samipjain1216@gmail.com>
2020-05-02 19:41:17 -0400

    Check if one or zero edits away

---
+++ day2/checkoneeditaway.py
@@ -0,0 +1,29 @@
+# You can perform insert, replace or remove a character from a string
+# Given two strings, check if they are one edit(or zero edit) away
+# "pale", "ple" = True
+# "pale", "bale" = True
+# "pale", "pales" = True
+# "pale", "bake" = False
+
+str1 = "pale"
+str2 = "ple"
+
+count = 0
+
+# Replace
+if (len(str1) == len(str2)):
+    for i in range(len(str1)):
+        if (str1[i] != str2[i]):
+            count += 1
+else:
+    diff = len(str1) - len(str2)
+    if (abs(diff) > 1):
+        count = diff
+
+if (count > 1):
+    print("False")
+else: 
+    print("True")
+
+
+
