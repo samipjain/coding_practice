509125d0aa87e2e45bbd707b38be95aed03b2f47
Samip Jain <samipjain1216@gmail.com>
2020-05-08 19:07:00 -0400

    Decode string - part1

---
+++ day4/decodestring.py
@@ -0,0 +1,24 @@
+def decodeString(s):
+    """
+    :type s: str
+    :rtype: str
+    """
+    stack = []
+    res = ""
+    for ch in s:
+        if ch != "]":
+            stack.append(ch)
+        else:
+            temp = stack[-1]
+            curr_ch = ""
+            while(temp != "["):
+                curr_ch = stack.pop() + curr_ch
+                temp = stack[-1]
+            remov_open_bracket = stack.pop()
+            num = stack.pop()
+            res += int(num) * curr_ch
+    return res
+        
+if __name__ == "__main__":
+    expr = "3[a2[c]]"
+    print(decodeString(expr))
