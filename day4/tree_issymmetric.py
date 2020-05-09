c2ae9da78bcfd1edd5dc4807761495ebff460d53
Samip Jain <samipjain1216@gmail.com>
2020-05-09 15:58:33 -0400

    Updates

--- day4/tree_issymmetric.py
+++ day4/tree_issymmetric.py
@@ -6,7 +6,7 @@ class Node:
 
 def isMirror(root1, root2):
     if (root1 is None and root2 is None):
-        return True
+        return 
     if root1 is not None and root2 is not None:
         if root1.val == root2.val:
             return isMirror(root1.left, root2.right) and isMirror(root1.right, root2.left)
