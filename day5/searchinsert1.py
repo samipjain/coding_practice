8cce13a27c05d7b203a0326e79e04c402b912c79
Samip Jain <samipjain1216@gmail.com>
2020-05-09 23:16:43 -0400

    Search Insert Position

---
+++ day5/searchinsert1.py
@@ -0,0 +1,14 @@
+def searchinsert(nums, target):
+    for i in range(len(nums)):
+        if(nums[i]==target):
+            return i
+    for j in range(len(nums)):
+        if(target<nums[j]):
+            return j
+        
+    return len(nums)
+
+if __name__ == "__main__":
+    arr = [1,3,5,6]
+    target = 5
+    print(searchinsert(arr, target))
