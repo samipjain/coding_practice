15df16fd5484270f2a5ad343bb4ce9ad381d213e
Samip Jain <samipjain1216@gmail.com>
2020-06-09 00:35:19 -0400

    Add BFS with visited array implementation

--- day10/graph_bfs.py
+++ day10/graph_bfs.py
@@ -25,11 +25,28 @@ class Graph:
                 queue.append(i)
         print(result)
 
+    def BFS_2(self, start):
+        queue = []
+        visited = [False for i in range(self.V)]
+        queue.append(start)
+        visited[start] = True
+
+        while queue:
+            element = queue.pop(0)
+            print(element)
+
+            for i in self.graph[element]:
+                if visited[i] is not True:
+                    queue.append(i)
+                    visited[i] = True
+
 if __name__ == "__main__":
     graph = Graph(4)
     graph.add_edge(0, 1)
+    graph.add_edge(0, 2)
     graph.add_edge(1, 2)
     graph.add_edge(2, 0)
     graph.add_edge(2, 3)
     graph.print_graph()
     graph.BFS(2)
+    graph.BFS_2(2)
