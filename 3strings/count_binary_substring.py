5bfb0fd30428566f64fddb93f879c812cd07a6f9
Samip Jain <samipjain1216@gmail.com>
2020-06-21 00:24:03 -0400

    Count Binary Substrings

--- 3strings/count_binary_substring.py
+++ 3strings/count_binary_substring.py
@@ -1,13 +1,26 @@
 def isValid(s):
+    prev = s[0]
+    i = 0
+    
     count_0 = 0
     count_1 = 0
-    # print("inside", s)
-    for char in s:
+    if prev == '0':
+        count_0 += 1
+    else:
+        count_1 += 1
+
+    for char in s[1:]:
+        if char != prev:
+            prev = char
+            i += 1
+        # dict_s[char] += 1
         if char == '0':
             count_0 += 1
         elif char == '1':
             count_1 += 1
     if count_0 != count_1:
+        return False
+    if i != 1:
         return False
     return True
     
@@ -20,11 +33,11 @@ def countBinarySubstrings(s):
     for i in range(len(s)):
         for j in range(i+1, len(s)+1):
             if isValid(s[i:j]):
-                print(s[i:j])
+                # print(s[i:j])
                 count += 1
     return count
 
 if __name__ == "__main__":
     print(countBinarySubstrings('00110'))
-    # print(countBinarySubstrings('00110011'))
-    # print(countBinarySubstrings('10101'))
+    print(countBinarySubstrings('00110011'))
+    print(countBinarySubstrings('10101'))
