56cf66a8829540f432fb5bee337e009ddabd13e0
Samip Jain <samipjain1216@gmail.com>
2020-06-20 20:02:54 -0400

    Strings

---
+++ 3strings/add_strings.py
@@ -0,0 +1,10 @@
+def addStrings(self, num1, num2):
+    """
+    :type num1: str
+    :type num2: str
+    :rtype: str
+    """
+    return str(int(num1)+int(num2))
+
+if __name__ == "__main__":
+    print(addStrings('11', '1'))
