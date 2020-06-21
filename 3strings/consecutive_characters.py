28b5056440c7db41525574854f71c91e6048c82f
Samip Jain <samipjain1216@gmail.com>
2020-06-20 21:45:01 -0400

    Add problem statement

--- 3strings/consecutive_characters.py
+++ 3strings/consecutive_characters.py
@@ -1,3 +1,12 @@
+"""
+Given a string s, the power of the string is the maximum length of a non-empty substring that contains only one unique character.
+
+Return the power of the string.
+Input: s = "abbcccddddeeeeedcba"
+Output: 5
+Explanation: The substring "eeeee" is of length 5 with the character 'e' only.
+"""
+
 def maxPower(s):
     """
     :type s: str
