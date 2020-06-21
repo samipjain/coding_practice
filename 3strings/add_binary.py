c854908ce18ffd520a3a8282805b71d82eff40b3
Samip Jain <samipjain1216@gmail.com>
2020-06-20 20:03:32 -0400

    remove extra function

--- 3strings/add_binary.py
+++ 3strings/add_binary.py
@@ -8,15 +8,5 @@ def addBinary(a, b):
     add = str(bin(int(a, 2) + int(b, 2)))
     return add[2:]
 
-def addBinary2(a, b):
-    """
-    :type a: str
-    :type b: str
-    :rtype: str
-    """
-    # Obvious solution
-    add = str(bin(int(a, 2) + int(b, 2)))
-    return add[2:]
-
 if __name__ == "__main__":
-    print(addBinary2('11', '1'))
+    print(addBinary('11', '1'))
