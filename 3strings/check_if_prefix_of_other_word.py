8d44c3309b1b599c82dc8df51526f7f9a26edf03
Samip Jain <samipjain1216@gmail.com>
2020-06-20 21:19:22 -0400

    Add better code

--- 3strings/check_if_prefix_of_other_word.py
+++ 3strings/check_if_prefix_of_other_word.py
@@ -13,6 +13,17 @@ def isPrefixOfWord(sentence, searchWord):
             return i+1
     return -1
 
+def isPrefixOfWord2(sentence, searchWord):
+    word_list = sentence.split(' ')
+    len_searchWord = len(searchWord)
+    # check each word if it has prefix
+    for i in range(len(word_list)):
+        if word_list[i][:len_searchWord] == searchWord:
+            return i+1
+    return -1
+
 if __name__ == "__main__":
     print(isPrefixOfWord("i love eating burger", "burg"))
     print(isPrefixOfWord("hellohello hellohellohello", "ell"))
+    print(isPrefixOfWord2("i love eating burger", "burg"))
+    print(isPrefixOfWord2("hellohello hellohellohello", "ell"))
