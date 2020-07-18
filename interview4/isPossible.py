b148919934da23c666bae518ccceced698bef11e
Samip Jain <samipjain1216@gmail.com>
2020-07-18 19:45:50 -0400

    Is possible

--- interview4/isPossible.py
+++ interview4/isPossible.py
@@ -9,3 +9,26 @@ input = (1,1,5,2)
 (1,1) => (1,2) => (3,2) => (5,2) 
 '''
 
+def isPossible(a, b, c, d):
+    if a > c or b > d:
+        return "No"
+    
+    if a == c and b == d:
+        return "Yes"
+
+    candidates = [(a,b)]
+
+    while len(candidates) != 0:
+        new_candidates = []
+        for data in candidates:
+            if data[0] == c and data[1] == d:
+                return "Yes"
+            
+            new_candidates.append((data[0]+data[1], data[1]))
+            new_candidates.append((data[0], data[0]+data[1]))
+        candidates = new_candidates
+    
+    return "No"
+
+if __name__ == "__main__":
+    print(isPossible(1,1,3,3))
