3d11e0133dd8705660c68e0ec0c075159a2323cc
Samip Jain <samipjain1216@gmail.com>
2020-07-18 19:31:45 -0400

    Keypad time

--- interview4/keypad.py
+++ interview4/keypad.py
@@ -12,3 +12,58 @@ keypad = '123456789'
 key password = '25679'
 output = 0 + 1 + 1 + 2 + 2 = 6
 '''
+
+def addNeighbors(x, y, keypad_list):
+    result = []
+
+    result.append(keypad_list[x-1][y-1])
+    result.append(keypad_list[x-1][y])
+    result.append(keypad_list[x-1][y+1])
+    result.append(keypad_list[x][y-1])
+    result.append(keypad_list[x][y+1])
+    result.append(keypad_list[x+1][y-1])
+    result.append(keypad_list[x+1][y])
+    result.append(keypad_list[x+1][y+1])
+    
+    return result
+
+def keypadTime(s, keypad):
+    neighbors = dict()
+
+    keypad_list = [[-1 for i in range(5)]]
+    keypad_temp_list = []
+    counter = 1
+
+    for ch in keypad:
+        if counter%5 == 1:
+            keypad_temp_list.append(-1)
+            keypad_temp_list.append(ch)
+            counter += 1
+        elif counter%4 == 0:
+            keypad_temp_list.append(ch)
+            keypad_temp_list.append(-1)
+            keypad_list.append(keypad_temp_list)
+            keypad_temp_list = []
+            counter = 0
+        else:
+            keypad_temp_list.append(ch)
+        counter += 1
+    
+    keypad_list.append([-1 for i in range(5)])
+    print(keypad_list)
+    
+    for x in range(1,4):
+        for y in range(1,4):
+            neighbors[keypad_list[x][y]] = addNeighbors(x, y, keypad_list)
+    time = 0
+    for i in range(1, len(s)):
+        if s[i] == s[i-1]:
+            continue
+        if s[i] not in neighbors[s[i-1]]:
+            time += 2
+        else:
+            time += 1
+    return time
+
+if __name__ == "__main__":
+    print(keypadTime('1234', '923857614'))
