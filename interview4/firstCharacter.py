861c5ea0264974666d9b79c38e46d6fc285f5055
Samip Jain <samipjain1216@gmail.com>
2020-07-18 19:06:27 -0400

    First repeating character

--- interview4/firstCharacter.py
+++ interview4/firstCharacter.py
@@ -2,3 +2,21 @@
 Find the first repeating character from a given string
 str = 'abcba', output = 'a' (not 'b')
 '''
+
+def firstCharacter(a):
+    characters = []
+    repeated = []
+
+    for ch in a:
+        if ch not in characters:
+            characters.append(ch)
+        else:
+            repeated.append(ch)
+
+    for ch in characters:
+        if ch in repeated:
+            return ch
+    return False
+
+if __name__ == "__main__":
+    print(firstCharacter('abcba'))
