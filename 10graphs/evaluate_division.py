48fcb9d6a5981c79516f09d718549c0fda9d55c4
Samip Jain <samipjain1216@gmail.com>
2020-07-26 17:41:41 -0400

    Evaluate Division

--- 10graphs/evaluate_division.py
+++ 10graphs/evaluate_division.py
@@ -15,7 +15,8 @@ class Graph:
             if dst not in self.V:
                 self.V.append(dst)
         
-            self.graph[src].append({dst, weight})
+            self.graph[src].append([dst, weight])
+            self.graph[dst].append([src, 1/weight])
 
     def printGraph(self):
         print(self.graph)
@@ -25,9 +26,39 @@ class Graph:
 
     def getEdges(self):
         return [self.graph[v] for v in self.V]
+    
+    def getWeight(self, src, dst):
+        # print("src", src, "dest", dst)
+        queue = []
+        queue.append(src)
+        result = 1
+
+        while queue:
+            curr = queue.pop(0)
+            if curr in self.graph:
+                for adj in self.graph[curr]:
+                    queue.append(adj[0])
+                    print("resutl", adj[0], adj[1])
+                    result = result * adj[1]
+                    if adj[0] == dst:
+                        break
+                        
+        return result
+
 
     def calcEquation(self, equations, values, queries):
+        answer =  [0] * len(queries)
+        
         self.addWeightedEdges(equations, values)
+
+        for i, query in enumerate(queries):
+            if query[0] == query[1]:
+                answer[i] = -1.0 if query[0] not in self.V else 1.0
+            elif query[0] not in self.V or query[1] not in self.V:
+                answer[i] = -1.0
+            else:
+                answer[i] = self.getWeight(query[0], query[1])
+        return answer
 
     
 if __name__ == "__main__":
@@ -35,9 +66,8 @@ if __name__ == "__main__":
 
     equations = [ ["a", "b"], ["b", "c"] ]
     values = [2.0, 3.0]
-    queries = [ ["a", "c"], ["b", "a"], ["a", "e"], ["a", "a"], ["x", "x"] ]
+    # queries = [ ["a", "c"], ["b", "a"], ["a", "e"], ["a", "a"], ["x", "x"] ]
+    queries = [ ["a", "c"] ]
 
-    g.calcEquation(equations, values, queries)
-
-    g.printGraph()
+    print(g.calcEquation(equations, values, queries))
     
