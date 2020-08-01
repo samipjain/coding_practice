295136565756f61eddb450cb41d834223a6e077e
Samip Jain <samipjain1216@gmail.com>
2020-08-01 18:53:12 -0400

    Change XY

---
+++ 9recursion/change_xy.py
@@ -0,0 +1,20 @@
+'''
+Given a string, compute recursively (no loops) a new string where all the lowercase 'x' chars have been changed to 'y' chars.
+
+
+changeXY("codex") → "codey"
+changeXY("xxhixx") → "yyhiyy"
+changeXY("xhixhix") → "yhiyhiy"
+'''
+
+def changeXY(s):
+    if s == '':
+        return ''
+    if s[0] == 'x':
+        return 'y'+changeXY(s[1:])
+    return s[0]+changeXY(s[1:])
+    
+
+print(changeXY('codex'))
+print(changeXY("xxhixx"))
+print(changeXY("xhixhix"))
