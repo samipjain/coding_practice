2bfbf567ba4be8d377e9a56eeb6aa8554c421b97
Samip Jain <samipjain1216@gmail.com>
2020-08-01 18:43:39 -0400

    Count hi

---
+++ 9recursion/count_hi.py
@@ -0,0 +1,20 @@
+'''
+
+Given a string, compute recursively (no loops) the number of times lowercase "hi" appears in the string.
+
+
+countHi("xxhixx") → 1
+countHi("xhixhix") → 2
+countHi("hi") → 1
+'''
+
+def counthi(a):
+    print("str ", a)
+    if a == '':
+        return 0
+    if a[0] == 'h' and a[1] == 'i':
+        return 1 + counthi(a[2:])
+    else:
+        return counthi(a[1:])
+
+print(counthi('ihxhixhiasxhih'))
