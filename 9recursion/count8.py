26076a889f1d76a4ebcd424060c85fa68761940a
Samip Jain <samipjain1216@gmail.com>
2020-08-01 18:28:45 -0400

    Count 8

---
+++ 9recursion/count8.py
@@ -0,0 +1,24 @@
+'''
+Given a non-negative int n, compute recursively (no loops) the count of the occurrences of 8 as a digit,
+ except that an 8 with another 8 immediately to its left counts double, so 8818 yields 4. Note that mod (%) by 10 
+ yields the rightmost digit (126 % 10 is 6), while divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
+
+
+count8(8) → 1
+count8(818) → 2
+count8(8818) → 4
+'''
+
+def count8(n):
+    return count8Util(n, 0, 0)
+
+def count8Util(n, prev, count):
+    if n == 0:
+        return count
+    if n%10 == 8:
+        count += 1 
+    if prev == 8 and n%10==8:
+        count += 1
+    return count8Util(n//10, n%10, count)
+
+print(count8(1088))
