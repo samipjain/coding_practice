ffe188c6d40141617cfb7d2d52ec8825622f8658
Samip Jain <samipjain1216@gmail.com>
2020-08-01 16:19:00 -0400

    Balanced binary tree - add comment

--- 6trees/balanced_binary_tree.py
+++ 6trees/balanced_binary_tree.py
@@ -1,3 +1,11 @@
+'''
+Given a binary tree, determine if it is height-balanced.
+
+For this problem, a height-balanced binary tree is defined as:
+
+a binary tree in which the left and right subtrees of every node differ in height by no more than 1.
+'''
+
 class Tree:
     def __init__(self, val):
         self.val = val
