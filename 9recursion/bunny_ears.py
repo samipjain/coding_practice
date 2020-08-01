50fefc64beb888430c0556aff1656da47dde34b7
Samip Jain <samipjain1216@gmail.com>
2020-08-01 17:17:40 -0400

    bunny ears 2

--- 9recursion/bunny_ears.py
+++ 9recursion/bunny_ears.py
@@ -13,4 +13,23 @@ def bunnyEars(n):
         return n
     return 2 + bunnyEars(n-1)
 
+'''
+We have bunnies standing in a line, numbered 1, 2, ... The odd bunnies (1, 3, ..) have the normal 2 ears. 
+The even bunnies (2, 4, ..) we'll say have 3 ears, because they each have a raised foot. Recursively return 
+the number of "ears" in the bunny line 1, 2, ... n (without loops or multiplication).
+
+
+bunnyEars2(0) → 0
+bunnyEars2(1) → 2
+bunnyEars2(2) → 5
+'''
+def bunnyEars2(n):
+    if n == 0:
+        return 0
+    if n%2 == 1:
+        return 2 + bunnyEars2(n-1)
+    else:
+        return 3 + bunnyEars2(n-1)
+
 print(bunnyEars(4))
+print(bunnyEars2(4))
