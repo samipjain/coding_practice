6aef1de2d43a6480e5752de63dd55d312d078e2a
Samip Jain <samipjain1216@gmail.com>
2020-08-25 00:35:56 -0400

    add edge cases

--- 1arrays/subarray_with_given_sum.py
+++ 1arrays/subarray_with_given_sum.py
@@ -1,4 +1,6 @@
 def givenSum(arr, sum):
+    if len(arr) == 0:
+        print("None")
     
     for i in range(0, len(arr)):
         total = 0
@@ -9,9 +11,13 @@ def givenSum(arr, sum):
                 break
         if total == sum:
             break
-
-    print(i+1, " ", j)
+    
+    if total == sum:
+        print(i+1, " ", j)
+    else:
+        print(-1)
 
 if __name__ == "__main__":
     givenSum([1,2,3,7,5], 12)
     givenSum([1,2,3,4,5,6,7,8,9,10], 15)
+    givenSum([1,2,3,7,5], 76)
